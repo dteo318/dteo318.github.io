e30c04603386b3e31ff79c9a0c81ca35a67e17f0
Daniel Teo <dteo318@gmail.com>
2025-05-22 16:50:01 +0800

    Navigation bar is sticky and animated

--- portfolio-app/src/components/NavBar.tsx
+++ portfolio-app/src/components/NavBar.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { ModeToggle } from "./ModeToggle";
 
 const NavListItem = ({ children }: { children: React.ReactNode }) => {
@@ -9,29 +12,46 @@ const NavListItem = ({ children }: { children: React.ReactNode }) => {
 };
 
 export default function NavBar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="flex items-center justify-center p-4 gap-4">
-      <ul className="flex gap-6 py-4 px-6 rounded-4xl font-semibold bg-stone-50 text-zinc-800 shadow-sm dark:bg-zinc-800 dark:text-zinc-400">
-        <NavListItem>
-          <a href="#home">Home</a>
-        </NavListItem>
-        <NavListItem>
-          <a href="#about">About</a>
-        </NavListItem>
-        <NavListItem>
-          <a href="#experience">Experience</a>
-        </NavListItem>
-        <NavListItem>
-          <a href="#projects">Projects</a>
-        </NavListItem>
-        <NavListItem>
-          <a href="#skills">Skills</a>
-        </NavListItem>
-        <NavListItem>
-          <a href="#contact">Contact</a>
-        </NavListItem>
-      </ul>
-      <ModeToggle />
-    </nav>
+    <div className="min-h-20">
+      <nav
+        className={`flex items-center justify-center gap-4 p-4 z-50 w-full fixed transition-transform duration-300 ease-in-out ${
+          isScrolled ? "translate-y-2" : "-translate-y-0"
+        }`}
+      >
+        <ul className="flex gap-6 py-4 px-6 rounded-4xl font-semibold bg-stone-50 text-zinc-800 shadow-sm dark:bg-zinc-800 dark:text-zinc-400">
+          <NavListItem>
+            <a href="#home">Home</a>
+          </NavListItem>
+          <NavListItem>
+            <a href="#about">About</a>
+          </NavListItem>
+          <NavListItem>
+            <a href="#experience">Experience</a>
+          </NavListItem>
+          <NavListItem>
+            <a href="#projects">Projects</a>
+          </NavListItem>
+          <NavListItem>
+            <a href="#skills">Skills</a>
+          </NavListItem>
+          <NavListItem>
+            <a href="#contact">Contact</a>
+          </NavListItem>
+        </ul>
+        <ModeToggle />
+      </nav>
+    </div>
   );
 }
