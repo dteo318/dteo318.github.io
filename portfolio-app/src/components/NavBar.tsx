a05b972faa0756582c4ebd3033e37066b703f810
Daniel Teo <dteo318@gmail.com>
2025-05-23 12:06:25 +0800

    Added scrollspy to highlight nav bar based on section in focus

--- portfolio-app/src/components/NavBar.tsx
+++ portfolio-app/src/components/NavBar.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { ModeToggle } from "./ModeToggle";
+import ScrollSpy from "react-scrollspy-navigation";
 
 const NavListItem = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -25,30 +26,32 @@ export default function NavBar() {
 
   return (
     <div className="min-h-20">
-      <nav
-        className={`flex items-center justify-center gap-4 p-4 z-50 w-full fixed transition-transform duration-300 ease-in-out ${
-          isScrolled ? "translate-y-2" : "-translate-y-0"
-        }`}
-      >
-        <ul className="flex gap-6 py-4 px-6 rounded-4xl font-semibold bg-stone-50 text-zinc-800 shadow-sm dark:bg-zinc-800 dark:text-zinc-400">
-          <NavListItem>
-            <a href="#home">Home</a>
-          </NavListItem>
-          <NavListItem>
-            <a href="#about">About</a>
-          </NavListItem>
-          <NavListItem>
-            <a href="#experience">Experience</a>
-          </NavListItem>
-          <NavListItem>
-            <a href="#projects">Projects</a>
-          </NavListItem>
-          <NavListItem>
-            <a href="#contact">Contact</a>
-          </NavListItem>
-        </ul>
-        <ModeToggle />
-      </nav>
+      <ScrollSpy activeClass="text-red-500 dark:text-teal-500">
+        <nav
+          className={`flex items-center justify-center gap-4 p-4 z-50 w-full fixed transition-transform duration-300 ease-in-out ${
+            isScrolled ? "translate-y-2" : "-translate-y-0"
+          }`}
+        >
+          <ul className="flex gap-6 py-4 px-6 rounded-4xl font-semibold bg-stone-50 text-zinc-800 shadow-sm dark:bg-zinc-800 dark:text-zinc-400">
+            <NavListItem>
+              <a href="#home">Home</a>
+            </NavListItem>
+            <NavListItem>
+              <a href="#about">About</a>
+            </NavListItem>
+            <NavListItem>
+              <a href="#experience">Experience</a>
+            </NavListItem>
+            <NavListItem>
+              <a href="#projects">Projects</a>
+            </NavListItem>
+            <NavListItem>
+              <a href="#contact">Contact</a>
+            </NavListItem>
+          </ul>
+          <ModeToggle />
+        </nav>
+      </ScrollSpy>
     </div>
   );
 }
