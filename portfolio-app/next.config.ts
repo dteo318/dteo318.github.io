52f18af3d81a3182d41388ecce3070146a0aec42
Daniel Teo <dteo318@gmail.com>
2025-05-24 18:42:51 +0800

    Updated assetPrefix in next js config

--- portfolio-app/next.config.ts
+++ portfolio-app/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  assetPrefix: "./",
+  assetPrefix: "/portfolio-app/",
   turbopack: {
     rules: {
       "*.svg": {
